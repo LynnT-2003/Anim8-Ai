795d29c808e2293e5c0f4f71dc3b141fc12391c8
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-11-07 23:44:01 +0700

    Updated UI

--- app/page.tsx
+++ app/page.tsx
@@ -3,29 +3,6 @@ import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { ChevronDown } from "lucide-react";
 
-const frameworks = [
-  {
-    value: "next.js",
-    label: "Next.js",
-  },
-  {
-    value: "sveltekit",
-    label: "SvelteKit",
-  },
-  {
-    value: "nuxt.js",
-    label: "Nuxt.js",
-  },
-  {
-    value: "remix",
-    label: "Remix",
-  },
-  {
-    value: "astro",
-    label: "Astro",
-  },
-];
-
 export default function Home() {
   const [openDuration, setOpenDuration] = useState(false);
 
@@ -42,16 +19,69 @@ export default function Home() {
   }, [content, style, duration]);
 
   return (
-    <div className="mt-12 mx-4">
+    <div className="mt-16 mx-4 flex flex-col items-center justify-center">
       {/* <h1 className="text-3xl text-center">Create New</h1> */}
       <div className="mx-24">
-        <h1 className="text-2xl">Content</h1>
-        <h1 className="mt-8">What is the topic of your video?</h1>
-        <div className="mt-4 rounded-2xl w-full py-32 border border-white"></div>
+        <h1 className="text-3xl font-bold">Content</h1>
+        <h1 className="mt-2">What is the topic of your video?</h1>
+        {/* <div className="mt-4 rounded-2xl w-full py-32 border border-white"></div> */}
+        <div className="flex space-x-4 mt-4">
+          <div
+            className={`${
+              content === "historical" ? "grayscale-0" : "grayscale"
+            } grayscale hover:grayscale-0 w-[300px] h-[400px] bg-gray-600 rounded-xl overflow-hidden transition duration-300 ease-in-out`}
+            onClick={() => {
+              setContent("historical");
+            }}
+          >
+            <img
+              src="/content/historical.jpg"
+              alt="historical"
+              className="w-full h-[350px] object-cover"
+            />
+            <h1 className="flex items-center justify-center h-[50px] text-xl font-bold">
+              Historical
+            </h1>
+          </div>
+          <div
+            className={`${
+              content === "motivational" ? "grayscale-0" : "grayscale"
+            } grayscale hover:grayscale-0 w-[300px] h-[400px] bg-gray-600 rounded-xl overflow-hidden transition duration-300 ease-in-out`}
+            onClick={() => {
+              setContent("motivational");
+            }}
+          >
+            <img
+              src="/content/motivational.jpg"
+              alt="motivational"
+              className="w-full h-[350px] object-cover"
+            />
+            <h1 className="flex items-center justify-center h-[50px] text-xl font-bold">
+              Motivational
+            </h1>
+          </div>
+          <div
+            className={`${
+              content === "fantasy" ? "grayscale-0" : "grayscale"
+            } grayscale hover:grayscale-0 w-[300px] h-[400px] bg-gray-600 rounded-xl overflow-hidden transition duration-300 ease-in-out`}
+            onClick={() => {
+              setContent("fantasy");
+            }}
+          >
+            <img
+              src="/content/fantasy.jpg"
+              alt="fantasy"
+              className="w-full h-[350px] object-cover"
+            />
+            <h1 className="flex items-center justify-center h-[50px] text-xl font-bold">
+              Fictional Fantasy
+            </h1>
+          </div>
+        </div>
       </div>
       <div className="mx-24 mt-16">
-        <h1 className="text-2xl">Style</h1>
-        <h1 className="mt-8 text-lg">What is the style of your video?</h1>
+        <h1 className="text-3xl font-bold">Art Style</h1>
+        <h1 className="mt-2">What is the style of the art in your video?</h1>
         <div className="flex space-x-4 mt-4">
           <div
             className={`${
@@ -108,8 +138,8 @@ export default function Home() {
       </div>
 
       <div className="mx-24 mt-16">
-        <h1 className="text-2xl">Duration</h1>
-        <h1 className="mt-8">What is the duration of your video?</h1>
+        <h1 className="text-3xl font-bold">Duration</h1>
+        <h1 className="mt-2">What is the duration of your video?</h1>
         <div
           className="mt-4 rounded-2xl w-[400px] py-3 border border-gray-600 flex items-center justify-between px-4 hover:cursor-pointer"
           onClick={() => {
@@ -163,7 +193,7 @@ export default function Home() {
         )}
       </div>
 
-      <div className="mx-24 mt-32 mb-16">
+      <div className="mx-24 mt-16 mb-16">
         <Button variant="secondary" className="text-xl py-8 w-full">
           Create short video
         </Button>
